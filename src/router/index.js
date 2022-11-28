2019290a6ae14ff276f26501de0f2bf4cd5041d5
Vladislav <umorphine@gmail.com>
2022-11-28 18:03:01 +0400

    [Fix] Deploy with new router history path

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainPageView from '../views/MainPageView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_APP_BASE_PATH),
   routes: [
     {
       path: '/',
